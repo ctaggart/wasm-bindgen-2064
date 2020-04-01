278d4d6f07d0f5a06ec85eb240be7bbe9728ba6a
Cameron Taggart <cameron.taggart@gmail.com>
2020-04-01 09:35:54 -0600

    apply fix

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,10 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub fn greet() {
     log("hello node");
-    // log(&ts.getNodeMajorVersion());
-    log(&getNodeMajorVersion());
-    // log(&version);
+    if let Some(major_node_version) = getNodeMajorVersion() {
+        log(&format!("Node major version: {}", &major_node_version));
+    }
+    log(&format!("TypeScript version: {}", &version.to_string()));
 }
 
 // https://rustwasm.github.io/docs/wasm-bindgen/examples/console-log.html
@@ -16,17 +17,8 @@ extern {
 }
 
 // https://rustwasm.github.io/docs/wasm-bindgen/reference/attributes/on-js-imports/module.html
-// default pr https://github.com/rustwasm/wasm-bindgen/pull/1106/files
 #[wasm_bindgen(module = "typescript")]
 extern {
-    fn getNodeMajorVersion() -> String;
-
-    // #[wasm_bindgen(js_name = default)]
-    // type Ts;
-    // static ts: Ts;
-
-    // #[wasm_bindgen(method)]
-    // fn getNodeMajorVersion(this: &Ts) -> String;
-
-    // static version: String;
+    fn getNodeMajorVersion() -> Option<u32>;
+    static version: String;
 }
